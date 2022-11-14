382a5ce7f19dd76ad92d25662622b4fa920a2319
Julio Facal <starlyn.dev@gmail.com>
2022-11-14 12:44:13 -0300

    Projeto inicial + estrutura model

---
+++ videos/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Videos(models.Model):
+    id = models.IntegerField()
+    titulo = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=1000)
+    url = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.titulo
