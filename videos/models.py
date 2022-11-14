b9ba7518d25a8d0a83f1138d2140038013811c52
Julio Facal <starlyn.dev@gmail.com>
2022-11-14 13:08:30 -0300

    fim semana 1

--- videos/models.py
+++ videos/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Videos(models.Model):
-    id = models.IntegerField(primary_key=True)
-    titulo = models.CharField(max_length=100)
-    descricao = models.CharField(max_length=1000)
-    url = models.CharField(max_length=200)
+    id = models.IntegerField(primary_key=True, null=False)
+    titulo = models.CharField(max_length=100, null=False)
+    descricao = models.CharField(max_length=1000, null=False)
+    url = models.CharField(max_length=200, null=False)
     
     def __str__(self):
         return self.titulo
